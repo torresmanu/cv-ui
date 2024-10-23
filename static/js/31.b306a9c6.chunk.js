2614b04278d2a19b5289c0cdf49926035c8d2803
torresmanu <manutorresschulten@frba.utn.edu.ar>
2024-10-23 22:46:24 +0200

    Updates

---
+++ static/js/31.b306a9c6.chunk.js
@@ -0,0 +1,20 @@
+(this.webpackJsonpCryptoVoice=this.webpackJsonpCryptoVoice||[]).push([[31],{1351:function(e,t,n){"use strict";n.r(t);var a=n(0),i=n.n(a),o=n(13),r=n(219),c=n(95),l=n(86);const m=o.c.div`
+  max-width: 520px;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  min-height: 100%;
+  padding-bottom: 5%;
+`,s=o.b`
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
+  body {
+    background: ${e=>e.theme.sidebar.header.background};
+  }
+`;t.default=function(){return i.a.createElement(m,null,i.a.createElement(s,null),i.a.createElement(r.a,{container:!0,direction:"row","justify-content":"center",alignItems:"center"},i.a.createElement(r.a,{item:!0},i.a.createElement(l.a,{logo:c.a}))))}}}]);
+//# sourceMappingURL=31.b306a9c6.chunk.js.map
